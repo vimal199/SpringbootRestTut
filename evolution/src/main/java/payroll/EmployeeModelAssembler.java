7082e7fc94cc91905ba3bee9ccf182e761417d88
Decodetalker <vimalbhardwaj8@gmail.com>
2026-08-24 23:53:02 +0530

    Fixed a compilation error

--- evolution/src/main/java/payroll/EmployeeModelAssembler.java
+++ evolution/src/main/java/payroll/EmployeeModelAssembler.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee,EntityModel<Employee>> {
 
     @Override
-    public EntityModel<Employee> toModel(Employee entity) {
+    public EntityModel<Employee> toModel(Employee employee) {
         return EntityModel.of(employee, //
                 linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
                 linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
